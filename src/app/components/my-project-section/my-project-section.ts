16f145678caa9d516fdd76ec01c2ee1b62b5d211
ArturGr <arturgroblicki@outlook.de>
2026-02-03 17:09:07 +0100

    Adding "my project" section - working on.

--- src/app/components/my-project-section/my-project-section.ts
+++ src/app/components/my-project-section/my-project-section.ts
@@ -1,4 +1,22 @@
 import { Component } from '@angular/core';
+import { MySkillsSection } from '../my-skills-section/my-skills-section';
+
+type ProjectList = {
+    id: number;
+    properties: {
+        duration: string;
+        name: string;
+        about: string;
+        workProcess: string;
+        groupWorkExperience: string;
+        technologies:string[];
+        technologiesIMG: string[];
+        technologiesALT:string[];
+        img: string;
+        git: string;
+        live: string;
+    };
+}[];
 
 @Component({
   selector: 'app-my-project-section',
@@ -6,6 +24,75 @@ import { Component } from '@angular/core';
   templateUrl: './my-project-section.html',
   styleUrl: './my-project-section.scss',
 })
-export class MyProjectSection {
+export class MyProjectSection extends MySkillsSection {
+
+  projects:ProjectList=[
+    {
+      id:1,
+      properties:{
+        duration:"3 weeks",
+        name:"El Pollo Loco",
+        about:"Dynamic 2D platformer game built with Vanilla JavaScript and HTML5 Canvas, featuring object-oriented programming, custom physics, and professional JSDoc documentation.",
+        workProcess:"How do you keep your code clean and maintainable? Have you broken the project down into reusable modules or components? Focus on documentation, naming files, variables, classes and testing.",
+        groupWorkExperience: "How many people were in the team and what was your role? Describe your tasks in 1-2 sentences, for example: login form, dashboard or chat functionality. What technologies did you use? It is nice to mention good teamwork and cooperation.",
+        technologies: [this.iconMobile[2][2],this.iconMobile[3][2],this.iconMobile[4][2],this.iconMobile[7][2]],
+        technologiesIMG: [this.iconMobile[2][0],this.iconMobile[3][0],this.iconMobile[4][0],this.iconMobile[7][0]],
+        technologiesALT:[this.iconMobile[2][1],this.iconMobile[3][1],this.iconMobile[4][1],this.iconMobile[7][1]],
+        img: "/img/projects/ElPolloLoco.png",
+        git:"https://github.com/ArturGr/El_Pollo_Loco.git",
+        live:"https://artur-groblicki.developerakademie.net/El_Pollo_Loco/index.html",
+      }
+    },
+        {
+      id:2,
+      properties:{
+        duration:"1 week",
+        name:"PokeDex",
+        about:"A responsive PokéDex application powered by the PokéAPI. Built with Vanilla JavaScript (ES6+), featuring asynchronous data fetching, dynamic search, and detailed Pokémon stat visualizations.",
+        workProcess:"How do you keep your code clean and maintainable? Have you broken the project down into reusable modules or components? Focus on documentation, naming files, variables, classes and testing.",
+        groupWorkExperience: "How many people were in the team and what was your role? Describe your tasks in 1-2 sentences, for example: login form, dashboard or chat functionality. What technologies did you use? It is nice to mention good teamwork and cooperation.",
+        technologies: [this.iconMobile[2][2],this.iconMobile[3][2],this.iconMobile[4][2],this.iconMobile[5][2]],
+        technologiesIMG: [this.iconMobile[2][0],this.iconMobile[3][0],this.iconMobile[4][0],this.iconMobile[5][0]],
+        technologiesALT:[this.iconMobile[2][1],this.iconMobile[3][1],this.iconMobile[4][1],this.iconMobile[5][1]],
+        img: "/img/projects/Pokedex.png",
+        git:"https://github.com/ArturGr/Pokedex.git",
+        live:"https://artur-groblicki.developerakademie.net/Pokedex/index.html",
+      }
+    },
+    {
+      id:3,
+      properties:{
+        duration:"1 week",
+        name:"Bestell App",
+        about:"A food ordering application focused on shopping cart logic, real-time price calculations, and dynamic DOM updates. Built with Vanilla JavaScript.",
+        workProcess:"How do you keep your code clean and maintainable? Have you broken the project down into reusable modules or components? Focus on documentation, naming files, variables, classes and testing.",
+        groupWorkExperience: "How many people were in the team and what was your role? Describe your tasks in 1-2 sentences, for example: login form, dashboard or chat functionality. What technologies did you use? It is nice to mention good teamwork and cooperation.",
+        technologies: [this.iconMobile[2][2],this.iconMobile[3][2],this.iconMobile[4][2],this.iconMobile[5][2]],
+        technologiesIMG: [this.iconMobile[2][0],this.iconMobile[3][0],this.iconMobile[4][0],this.iconMobile[5][0]],
+        technologiesALT:[this.iconMobile[2][1],this.iconMobile[3][1],this.iconMobile[4][1],this.iconMobile[5][1]],
+        img: "/img/projects/Bestellapp.png",
+        git:"https://github.com/ArturGr/Bestellapp.git",
+        live:"https://artur-groblicki.developerakademie.net/Bestellapp/index.html",
+      }
+    },
+    {
+      id:4,
+      properties:{
+        duration:"x weeks",
+        name:"JOIN - ongoing project",
+        about:"This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quae optio neque dignissimos eaque aliquid porro quaerat iusto repudiandae deleniti voluptatum harum, sapiente ipsa blanditiis ex nam eligendi. Amet, possimus!",
+        workProcess:"test",
+        groupWorkExperience: "test",
+        technologies: [this.iconMobile[0][2],this.iconMobile[1][2],this.iconMobile[5][2],this.iconMobile[6][2],this.iconMobile[7][2]],
+        technologiesIMG: [this.iconMobile[0][0],this.iconMobile[1][0],this.iconMobile[5][0],this.iconMobile[6][0],this.iconMobile[7][0]],
+        technologiesALT:[this.iconMobile[0][1],this.iconMobile[1][1],this.iconMobile[5][1],this.iconMobile[6][1],this.iconMobile[7][1]],
+        img: "#",
+        git:"#",
+        live:"#",
+      }
+    }
+  ]
+  activeProject:number = 1;
+
 
 }
--- src/app/components/my-skills-section/my-skills-section.ts
+++ src/app/components/my-skills-section/my-skills-section.ts
@@ -22,16 +22,16 @@ export class MySkillsSection extends MenuBar {
   ];
 
   iconMobile:string[][]=[
-    ["./img/skills/mobile/0-Angular.png","Angular Icon"],
-    ["./img/skills/mobile/1-TypeScript.png","TypeScript Icon"],
-    ["./img/skills/mobile/2-JavaScript.png","JavaScript Icon"],
-    ["./img/skills/mobile/3-HTML.png","HTML Icon"],
-    ["./img/skills/mobile/4-CSS.png","CSS Icon"],
-    ["./img/skills/mobile/5-Rest-Api.png","RestAPI Icon"],
-    ["./img/skills/mobile/6-Firebase.png","Firebase Icon"],
-    ["./img/skills/mobile/7-GIT.png","GIT Icon"],
-    ["./img/skills/mobile/8-Scrum.png","Scrum Icon"],
-    ["./img/skills/mobile/9-Material-Design.png","Material Design Icon"],
+    ["./img/skills/mobile/0-Angular.png","Angular Icon", "Angular"],
+    ["./img/skills/mobile/1-TypeScript.png","TypeScript Icon","TypeScript"],
+    ["./img/skills/mobile/2-JavaScript.png","JavaScript Icon","JavaScript"],
+    ["./img/skills/mobile/3-HTML.png","HTML Icon","HTML"],
+    ["./img/skills/mobile/4-CSS.png","CSS Icon","CSS"],
+    ["./img/skills/mobile/5-Rest-Api.png","RestAPI Icon","RestAPI"],
+    ["./img/skills/mobile/6-Firebase.png","Firebase Icon","Firebase"],
+    ["./img/skills/mobile/7-GIT.png","GIT Icon","GIT"],
+    ["./img/skills/mobile/8-Scrum.png","Scrum Icon","Scrum"],
+    ["./img/skills/mobile/9-Material-Design.png","Material Design Icon","Material Design"],
   ];
 
   currentlyLearning:string[][]=[
