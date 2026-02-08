caa193b1589902860e858c7326f6753e654e16f8
ArturGr <arturgroblicki@outlook.de>
2026-02-08 14:00:40 +0100

    Added "contact me" section

--- src/app/components/contact-form/contact-form.ts
+++ src/app/components/contact-form/contact-form.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.scss',
 })
 export class ContactForm {
+contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(5)]],
+      privacy: [false, Validators.requiredTrue]
+    });
+  }
+
+  isInvalid(controlName: string): boolean {
+    const control = this.contactForm.get(controlName);
+    return !!(control && control.invalid && (control.dirty || control.touched));
+  }
+
+  isValid(controlName: string): boolean {
+    const control = this.contactForm.get(controlName);
+    return !!(control && control.valid && (control.dirty || control.touched));
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Formularz wysłany:', this.contactForm.value);
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
+  }
 }
--- src/app/components/contact-me-section/contact-me-section.ts
+++ src/app/components/contact-me-section/contact-me-section.ts
@@ -1,6 +1,18 @@
 import { Component } from '@angular/core';
 import { ContactForm } from "../contact-form/contact-form";
 
+type Icon = {
+  email:{
+      default:string,
+      hover:string,
+      click:string
+    },
+  tel:{
+      default: string,
+      hover: string,
+    }
+}
+
 @Component({
   selector: 'app-contact-me-section',
   imports: [ContactForm],
@@ -8,5 +20,22 @@ import { ContactForm } from "../contact-form/contact-form";
   styleUrl: './contact-me-section.scss',
 })
 export class ContactMeSection {
+  icon:Icon={
+    email:{
+      default: "./img/contact/email/Default.png",
+      hover: "./img/contact/email/Hover.png",
+      click: "./img/contact/email/Click.png"
+    },
+    tel:{
+      default: "./img/contact/tel/Default.png",
+      hover: "./img/contact/tel/Hover.png",
+    }
+  }
+  emailStatus: 'default' | 'hover' | 'click' = 'default';
+  telStatus: 'default' | 'hover' = 'default';
 
+  handleEmailClick() {
+    this.emailStatus = 'click';
+    setTimeout(() => this.emailStatus = 'hover', 200);
+  }
 }
