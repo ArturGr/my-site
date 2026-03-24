ed8f0bc4dd555841d3e9a1fc96296f417640d284
ArturGr <arturgroblicki@outlook.de>
2026-03-24 10:19:00 +0100

    Better styling for responsive version.

--- src/app/components/my-references/my-references.ts
+++ src/app/components/my-references/my-references.ts
@@ -7,6 +7,7 @@ type References= {
         personName: string;
         projectName: string;
         description: string;
+        descriptionDE: string;
         linkedProfile: string;
     };
 }[];
@@ -23,9 +24,10 @@ export class MyReferences {
     {
       id:1,
       properties:{
-        personName:"Lorem ipsum",
-        projectName:"Lorem ipsum dolor",
-        description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea.",
+        personName:"Luis Kronhardt",
+        projectName:"Join",
+        description:"During our collaboration, Artur was very reliable and helpful. His knowledge and speed were particularly noteworthy. He was always willing to explain his ideas to his team and approached problems calmly and confidently.",
+        descriptionDE:"Während unserer Zusammenarbeit hat Artur sehr zuverlässig und hilfsbereit mitgearbeitet. Besonders stach sein Wissen und seine Schnelligkeit heraus. Er war stets bereit seinem Team seine Ideen zu erläutern und ging Probleme ruhig und gelassen an.",
         linkedProfile:"https://www.linkedin.com/",
       }
     },
@@ -33,8 +35,9 @@ export class MyReferences {
       id:2,
       properties:{
         personName:"Lorem ipsum",
-        projectName:"Lorem ipsum dolor",
+        projectName:"Join",
         description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea",
+        descriptionDE:"",
         linkedProfile:"https://www.linkedin.com/",
       }
     },
@@ -44,6 +47,7 @@ export class MyReferences {
         personName:"Lorem ipsum",
         projectName:"Lorem ipsum dolor",
         description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea",
+        descriptionDE:"",
         linkedProfile:"https://www.linkedin.com/",
       }
     },
